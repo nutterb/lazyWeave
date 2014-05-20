ad7a5b2735368dfbc13f16475ca1d2b52908dd69
Benjamin <benjamin.nutter@gmail.com>
2014-05-20 10:48:29 -0400

    Update setHtmlOptions.R

--- R/setHtmlOptions.R
+++ R/setHtmlOptions.R
@@ -2,14 +2,14 @@ setHtmlOptions <- function(table=NULL, figure=NULL, footnote=NULL,
                          chapter=NULL, section=NULL, subsection=NULL,
                          subsubsection=NULL, 
                          font.family=NULL, font=NULL, font.size=NULL){
-  if (!is.null(table)) assign("HTML.COUNTER.TABLE", table, envir=htmlCounters)
-  if (!is.null(figure)) assign("HTML.COUNTER.FIGURE", figure, envir=htmlCounters)
-  if (!is.null(footnote)) assign("HTML.COUNTER.FOOTNOTE", footnote, envir=htmlCounters)
-  if (!is.null(chapter)) assign("HTML.COUNTER.CHAPTER", chapter, envir=htmlCounters)
-  if (!is.null(section)) assign("HTML.COUNTER.SECTION", section, envir=htmlCounters)
-  if (!is.null(subsection)) assign("HTML.COUNTER.SUBSECTION", subsection, envir=htmlCounters)
-  if (!is.null(subsubsection)) assign("HTML.COUNTER.SUBSUBSECTION", subsubsection, envir=htmlCounters)
-  if (!is.null(font.family)) assign("HTML.FONT.FAMILY", font.family, envir=htmlCounters)
-  if (!is.null(font)) assign("HTML.FONT.FONT", font, envir=htmlCounters)
-  if (!is.null(font.size)) assign("HTML.FONT.SIZE", font.size, envir=htmlCounters)
+  if (!is.null(table)) assign("HTML.COUNTER.TABLE", table, envir=options()$htmlCounters)
+  if (!is.null(figure)) assign("HTML.COUNTER.FIGURE", figure, envir=options()$htmlCounters)
+  if (!is.null(footnote)) assign("HTML.COUNTER.FOOTNOTE", footnote, envir=options()$htmlCounters)
+  if (!is.null(chapter)) assign("HTML.COUNTER.CHAPTER", chapter, envir=options()$htmlCounters)
+  if (!is.null(section)) assign("HTML.COUNTER.SECTION", section, envir=options()$htmlCounters)
+  if (!is.null(subsection)) assign("HTML.COUNTER.SUBSECTION", subsection, envir=options()$htmlCounters)
+  if (!is.null(subsubsection)) assign("HTML.COUNTER.SUBSUBSECTION", subsubsection, envir=options()$htmlCounters)
+  if (!is.null(font.family)) assign("HTML.FONT.FAMILY", font.family, envir=options()$htmlCounters)
+  if (!is.null(font)) assign("HTML.FONT.FONT", font, envir=options()$htmlCounters)
+  if (!is.null(font.size)) assign("HTML.FONT.SIZE", font.size, envir=options()$htmlCounters)
 }
