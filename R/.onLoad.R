fc44340a1673cef1c5696bdf2804d87ddd9c47fb
Benjamin <benjamin.nutter@gmail.com>
2014-05-20 10:03:36 -0400

    Update .onLoad.R

--- R/.onLoad.R
+++ R/.onLoad.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
   options("lazyReportFormat" = "latex")
-  htmlCounters <- new.env(parent=globalenv())
+  assign("htmlCounters", new.env(), envir=.GlobalEnv)
   assign("HTML.COUNTER.TABLE", 1, envir=htmlCounters)
   assign("HTML.COUNTER.FIGURE", 1, envir=htmlCounters)
   assign("HTML.COUNTER.FOOTNOTE", 1, envir=htmlCounters)
