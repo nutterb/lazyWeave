dee9f309d8e32c178c5304e4882bb9bc0ddb2534
Benjamin <benjamin.nutter@gmail.com>
2014-05-20 10:50:19 -0400

    Update lazy.write.R

--- R/lazy.write.R
+++ R/lazy.write.R
@@ -35,9 +35,9 @@ function(..., OutFile, append=FALSE, collapse="\n", footnotes=TRUE){
   code <- paste(code, collapse = collapse)
   
   
-  if (reportFormat == "html" & footnotes & !is.null(get("HTML.FOOTNOTES", envir=htmlCounters))){
-    code <- gsub("</html>", paste("\n\n\n", get("HTML.FOOTNOTES", envir=htmlCounters), "\n</html>"), code)
-    assign("HTML.FOOTNOTES", NULL, envir=htmlCounters)
+  if (reportFormat == "html" & footnotes & !is.null(get("HTML.FOOTNOTES", envir=options()$htmlCounters))){
+    code <- gsub("</html>", paste("\n\n\n", get("HTML.FOOTNOTES", envir=options()$htmlCounters), "\n</html>"), code)
+    assign("HTML.FOOTNOTES", NULL, envir=options()$htmlCounters)
   }
   
   write(code, OutFile, append = append)
