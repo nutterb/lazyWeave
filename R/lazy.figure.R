bdcf0489a74b744fe8c8f1062eb4186432fe8fd6
Benjamin <benjamin.nutter@gmail.com>
2014-05-20 11:11:46 -0400

    Update lazy.figure.R

--- R/lazy.figure.R
+++ R/lazy.figure.R
@@ -27,7 +27,7 @@ function(filename, caption=NULL, align="center",
     width  <- paste("width=",  width, units, sep="")
    
     #*** Specify and set counter
-    counterStr <- if (!missing(counter)) paste("  ", lazy.counter(counter, fn="use"), sep="") else "%% \\usecounter{}\n"
+    counterStr <- if (!missing(counter)) paste("  ", lazy.counter("figure", fn="use"), sep="") else "%% \\usecounter{}\n"
     if (!is.null(counterSet)) counterStr <- paste(counterStr, "\n  ", lazy.counter(counter, value=counterSet - 1, fn="set"), sep="")
 
     #*** Set caption and label strings
