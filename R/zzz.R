10fcd7a8934c507107e2a2117b23ccfe00b00a60
Benjamin <benjamin.nutter@gmail.com>
2014-05-20 10:23:14 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,19 @@
 .onLoad <- function(libname, pkgname){
   options("lazyReportFormat" = "latex")
-  assign(".HTML.COUNTER.TABLE.", 1, envir=.GlobalEnv)
-  assign(".HTML.COUNTER.FIGURE.", 1, envir=.GlobalEnv)
-  assign(".HTML.COUNTER.FOOTNOTE.", 1, envir=.GlobalEnv)
-  assign(".HTML.COUNTER.CHAPTER.", 1, envir=.GlobalEnv)
-  assign(".HTML.COUNTER.SECTION.", 1, envir=.GlobalEnv)
-  assign(".HTML.COUNTER.SUBSECTION.", 1, envir=.GlobalEnv)
-  assign(".HTML.COUNTER.SUBSUBSECTION.", 1, envir=.GlobalEnv)
-  assign(".HTML.FOOTNOTES.", NULL, envir=.GlobalEnv)
-  assign(".HTML.FONT.FAMILY.", "serif", envir=.GlobalEnv)
-  assign(".HTML.FONT.FONT.", "helvetica", envir=.GlobalEnv)
-  assign(".HTML.FONT.SIZE.", 11, envir=.GlobalEnv)
+  assign("htmlCounters", new.env(), envir=.GlobalEnv)
+  assign("HTML.COUNTER.TABLE", 1, envir=htmlCounters)
+  assign("HTML.COUNTER.FIGURE", 1, envir=htmlCounters)
+  assign("HTML.COUNTER.FOOTNOTE", 1, envir=htmlCounters)
+  assign("HTML.COUNTER.CHAPTER", 1, envir=htmlCounters)
+  assign("HTML.COUNTER.SECTION", 1, envir=htmlCounters)
+  assign("HTML.COUNTER.SUBSECTION", 1, envir=htmlCounters)
+  assign("HTML.COUNTER.SUBSUBSECTION", 1, envir=htmlCounters)
+  assign("HTML.FOOTNOTES", NULL, envir=htmlCounters)
+  assign("HTML.FONT.FAMILY", "serif", envir=htmlCounters)
+  assign("HTML.FONT.FONT", "helvetica", envir=htmlCounters)
+  assign("HTML.FONT.SIZE", 11, envir=htmlCounters)
+}
+
+.onUnload <- function(libname, pkgname){
+  rm(htmlCounters)
 }
