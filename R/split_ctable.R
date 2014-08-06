8637feee08f477684c85e1d9b62d42a71444a526
Benjamin <benjamin.nutter@gmail.com>
2014-08-06 14:29:49 -0400

    Allow variables to be broken across pages
    The previous implementation assumed you would never want a factor variable to be broken over two pages.  This creates problems if you are using a factor that has more levels than max.rows.  Specifically, if you make a table of one variable or a table where the first variable has more levels than max.rows, if you try to keep the variable together, there will be no data to display on the first page.  split_ctable doesn't like this very much.  Setting keepVarTogether=FALSE forces exactly max.rows displayed on each page (except maybe the last)

--- R/split_ctable.R
+++ R/split_ctable.R
@@ -1,9 +1,14 @@
-split_ctable <- function(x, max.rows=35, ...){
+split_ctable <- function(x, max.rows=35, keepVarTogether=TRUE, ...){
   lazy.args <- list(...)
   if ("counter" %in% names(lazy.args)) counter <- lazy.args$counter else counter <- "table"
   rowList <- list()
   rows <- 1:nrow(x)
   if (length(rows) < max.rows) rowList <- list(rows)
+  else if (!keepVarTogether){
+    ntab <- ceiling(nrow(x) / max.rows)
+    index <- rep(1:ntab, rep(max.rows, ntab))[1:nrow(x)]
+    rowList <- lapply(unique(index), function(x) rows[index==x])
+  }
   else {
     while(length(rows) > max.rows){
       lastRow <- if (!is.na(x$name[rows[max.rows] + 1])) max.rows else max(which(!is.na(x$name[rows[1]:rows[max.rows]]))) - 1
