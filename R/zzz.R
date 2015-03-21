591ba6606ced57dd44d790d4cd705ee092ef393f
nutterb <benjamin.nutter@gmail.com>
2015-03-21 13:29:55 -0400

    Improvements toward version 3

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname){
-  options("lazyReportFormat" = "latex")
+  options("lazyReportFormat" = "markdown")
+  options("lazyWeave_cat" = TRUE)
   options("htmlCounters" = new.env())
   assign("HTML.COUNTER.TABLE", 1, envir=options()$htmlCounters)
   assign("HTML.COUNTER.FIGURE", 1, envir=options()$htmlCounters)
